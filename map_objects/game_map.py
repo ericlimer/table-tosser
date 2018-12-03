2aff8ada25fe4fe454d067341789bfa4c0594ff0
eric limer <ericdlimer@gmail.com>
2018-12-02 21:59:28 -0500

    items grabbable (but non-functional)

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -3,6 +3,7 @@ from random import randint
 
 from components.ai import BasicMonster
 from components.fighter import Fighter
+from components.item import Item
 
 from entity import Entity
 from render_functions import RenderOrder
@@ -127,8 +128,9 @@ class GameMap:
             x = randint(room.x1 + 1, room.x2 - 1)
             y = randint(room.y1 + 1, room.y2 - 1)
 
+            item_component = Item()
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM)
+                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
 
                 entities.append(item)
 
